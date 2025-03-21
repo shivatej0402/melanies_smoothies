aedbb9ec31e6b1fd64e2a428478d03a028ffd424
shivatej0402 <tejshiva422@gmail.com>
2025-03-21 12:42:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     INGREDIENTS_STRING = ''
     for fruit_chosen in ingredients_list:
-         INGREDIENTS_STRING += fruit_chosen  +' '
+         INGREDIENTS_STRING += fruit_chosen  +'  '
          st.subheader(fruit_chosen + 'Nutrition Information')
          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
          sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
